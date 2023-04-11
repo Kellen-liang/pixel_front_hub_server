8eacf114e498a8d29d7baa123f1b695e8b43c284
t_kellenliang <t_kellenliang@kugou.net>
2023-04-11 19:42:04 +0800

    feat:添加login/register请求

---
+++ app.js
@@ -0,0 +1,102 @@
+const express = require('express')
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { Op } = require("sequelize");
+const jwt = require("jsonwebtoken")
+const cookieParser = require('cookie-parser')
+
+//创建应用对象
+const PORT = 3001
+const app = express()
+
+// const corsOptions = {
+//   origin: "*"
+// }
+// app.use(cors(corsOptions));
+app.use(express.json())
+// content-type：application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// content-type：application/json
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+
+app.all('*', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:3002");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header("X-Powered-By", ' 3.2.1')
+  next();
+});
+
+
+const { User } = require('./entity/entity.js')
+
+
+app.post('/api/user/login', async (req, res) => {
+  const { username, password } = req.body
+  const user = await User.findOne({ where: { username: username } })
+  if (user === null) return res.status(200).json({ status: 0, message: '用户不存在' })
+  if (user.password !== password) return res.status(200).json({ status: 0, message: '密码错误' })
+
+  const token = jwt.sign({ id: user.id, username: username }, "jwtkey")
+  const { ...other } = user.dataValues
+
+  res
+    .cookie("access_token", token, {
+      httpOnly: true,
+    })
+    .status(200)
+    .json(other)
+})
+
+app.post('/api/user/get', async (req, res)=> {
+  console.log('--',req.cookies);
+})
+app.post('/api/user/register', async (req, res) => {
+  console.log('------------');
+  console.log(req.body);
+  const { username, password, email } = req.body
+
+
+  const [user, created] = await User.findOrCreate({
+    where: {
+      [Op.or]: [
+        { username: username },
+        { email: email },
+      ]
+    },
+    defaults: {
+      username: username,
+      password: password,
+      email: email
+    }
+  })
+  if (!created) return res.status(200).json({ status: 0, message: '该用户名或者邮箱已被注册' })
+  console.log('--user', user);
+  console.log('created---', created);
+
+})
+// app.get('/', async (req, res) => {
+
+//     // console.log(User === sequelize.models.User); // true
+//     const data = await User.findAll();
+//     console.log(data)
+//     res.send(data.map((item) => item.toJSON()));
+//   // 这里是代码
+// });
+
+// app.get('/set', async (req, res) => {
+//   const { username, password, email } = req.query;
+//   await User.create({
+//     username,
+//     password,
+//   })
+//   res.send('OK')
+// })
+
+//启动一个服务并监听从 3000端口进入的所有连接请求
+app.listen(PORT, function () {
+  console.log('服务器开启成功');
+});
---
+++ entity/entity.js
@@ -0,0 +1,179 @@
+const { DataTypes, Model } = require('sequelize')
+const { sequelize } = require('../db')
+const { v4: uuidv4 } = require("uuid")
+
+/**
+ * @description 创建ORM模型
+ */
+function createModel({ tableName, ...attributes }) {
+  class T extends Model { };
+  T.init(attributes, { sequelize, tableName });
+  T.sync({ alter: true });
+  return T;
+}
+
+//使用
+//用户实例
+const User = createModel({
+  tableName: "user",
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+    allowNull: false,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  posts: DataTypes.STRING,
+  company: DataTypes.STRING,
+  main_page_introduction: DataTypes.STRING,
+  introduction: DataTypes.STRING,
+})
+
+//订阅实例
+const Subscriber = createModel({
+  tableName: "subscriber",
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+    allowNull: false,
+  },
+  subscribe_user_id: {
+    type: DataTypes.UUID,
+    allowNull: false,
+  },
+
+  be_subscribe_user_id: {
+    type: DataTypes.UUID,
+    allowNull: false,
+  },
+
+})
+
+//文章实例
+const Article = createModel({
+  tableName: "article",
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+    allowNull: false,
+  },
+  user_id: {
+    type: DataTypes.UUID,
+    allowNull: false,
+  },
+  article_title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  article_cover: DataTypes.STRING,
+  article_intro: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  article_content: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  count: DataTypes.INTEGER,
+  likes: DataTypes.INTEGER,
+  comment_count: DataTypes.INTEGER,
+  tag: DataTypes.STRING,
+})
+
+//点赞和收藏实例
+const LikeCollectCount = createModel({
+  tableName: "like_collect_count",
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+    allowNull: false,
+  },
+  user_id: {
+    type: DataTypes.UUID,
+    allowNull: false,
+  },
+  collect_id: {
+    type: DataTypes.UUID,
+    allowNull: false,
+  },
+  like_id: {
+    type: DataTypes.UUID,
+    allowNull: false,
+  },
+  article_id: {
+    type: DataTypes.UUID,
+    allowNull: false,
+  }
+})
+
+//评论实例
+const Comment = createModel({
+  tableName: "comment",
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+    allowNull: false,
+  },
+  user_id: {
+    type: DataTypes.UUID,
+    allowNull: false,
+  },
+  article_id: {
+    type: DataTypes.UUID,
+    allowNull: false,
+  },
+  content: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+})
+
+//回复实例
+const Reply = createModel({
+  tableName: "reply",
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+    allowNull: false,
+  },
+  user_id: {
+    type: DataTypes.UUID,
+    allowNull: false,
+  },
+  article_id: {
+    type: DataTypes.UUID,
+    allowNull: false,
+  },
+  comment_id: {
+    type: DataTypes.UUID,
+    allowNull: false,
+  },
+  reply_late_id: {
+    type: DataTypes.UUID,
+    allowNull: false,
+  },
+  content: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+})
+
+
+
+module.exports = { User, Subscriber, Article, LikeCollectCount, Comment, Reply }
